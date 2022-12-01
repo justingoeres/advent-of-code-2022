d3ac5a40f1e9b7e4188103cdbe4f4647278ccad2
Justin Goeres <justin.goeres@merklecxm.com>
2022-12-01 18:13:22 -0500

    gitignore .js files in tests/

--- tests/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("../src/index");
-describe('testing index file', () => {
-    test('empty string should result in zero', () => {
-        expect((0, index_1.add)('')).toBe(0);
-    });
-});
