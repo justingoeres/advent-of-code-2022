7a856107a272506de147f608fdc96a941f0cef6b
Justin Goeres <justin.goeres@gmail.com>
2022-12-04 00:48:40 -0500

    Day 4: Camp Cleanup

--- src/2022/day04/Day04.ts
+++ src/2022/day04/Day04.ts
@@ -1,8 +1,11 @@
 import {LinesReader} from '../../common/reader/LinesReader';
+import {CsvFileReader} from '../../common/reader/CsvFileReader';
+import {Ranges} from './Ranges';
+import {ElfRange} from './ElfRange';
 
 export class Day04 {
     reader: LinesReader;
-    input: any;
+    ranges: Ranges[] = [];
 
     constructor(inputFile: string) {
         this.reader = new LinesReader(inputFile);
@@ -12,21 +15,27 @@ export class Day04 {
 
     doPart1(): number {
         /*
-            DESCRIPTION
+            In how many assignment pairs does one range fully contain the other?
         */
-
-        return 0;
+        const total: number = this.ranges
+            .filter((element) => element.hasContainment())
+            .length;
+        return total;
     }
 
     doPart2(): number {
         /*
-            DESCRIPTION
+            In how many assignment pairs do the ranges overlap?
         */
+        const total: number = this.ranges
+            .filter((element) => element.hasOverlap())
+            .length;
+        return total;
     }
 
     parseInputPart1(): void {
-        this.input = this.reader.lines.map((line: string): any => {
-            return 'something';
+        this.ranges = this.reader.lines.map((line: string): any => {
+            return new Ranges(line);
         });
     }
 
