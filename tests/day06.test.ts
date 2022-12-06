296ec99bf639af4cac45d27b970bb70c17874ffa
Justin Goeres <justin.goeres@merklecxm.com>
2022-12-06 00:26:13 -0500

    Day 6 Init from aocInit.sh

---
+++ tests/day06.test.ts
@@ -0,0 +1,24 @@
+import {Day06} from '../src/2022/day06/Day06';
+
+describe('Day 6: NAME', () => {
+
+    let day06: Day06 = new Day06('data/day06/input.txt');
+    let day06Example: Day06 = new Day06('data/day06/example.txt');
+
+    test('Example Part 1: PART1_RESULT', () => {
+        expect(day06Example.doPart1()).toBe(0);
+    });
+
+    test('Example Part 2: PART2_RESULT', () => {
+        expect(day06Example.doPart2()).toBe(0);
+    });
+
+    test('Part 1: PART1_RESULT', () => {
+        expect(day06.doPart1()).toBe(0);
+    });
+
+    test('Part 2: PART2_RESULT', () => {
+        expect(day06.doPart2()).toBe(0);
+    });
+});
+
