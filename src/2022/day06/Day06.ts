4f598ac912a822cc7c526c0b16f8845d0c666759
Justin Goeres <justin.goeres@merklecxm.com>
2022-12-06 10:57:28 -0500

    Day 6: Tuning Trouble Part 2

--- src/2022/day06/Day06.ts
+++ src/2022/day06/Day06.ts
@@ -1,5 +1,6 @@
 import {LinesReader} from '../../common/reader/LinesReader';
 import {SingleLineReader} from '../../common/reader/SingleLineReader';
+import {MapUtils} from '../../common/MapUtils';
 
 export class Day06 {
     reader: SingleLineReader;
@@ -16,22 +17,30 @@ export class Day06 {
             How many characters need to be processed before the first start-of-packet marker is detected?
         */
 
-        for (var i = 0; i < this.reader.line.length; i++) {
-            // get the next 4-character buffer
-            const buffer = this.reader.line.slice(i, i + 4);
+        // for (var i = 0; i < this.reader.line.length; i++) {
+        //     // get the next 4-character buffer
+        //     const buffer = this.reader.line.slice(i, i + 4);
+        //
+        //     // is it a valid start of packet?
+        //     if (this.isStartOfPacket(buffer)) break;
+        // }
+        // return i + 4;
 
-            // is it a valid start of packet?
-            if (this.isStartOfPacket(buffer)) break;
-        }
-        return i + 4;
+        const startPos: number = this.findStartOfPacket(4, this.reader.line);
+        return startPos;
     }
 
 
     doPart2(): number {
         /*
-            DESCRIPTION
+            Now a start-of-message marker is just like a start-of-packet marker,
+            except it consists of 14 distinct characters rather than 4.
+
+            How many characters need to be processed before the first start-of-packet marker is detected?
+
         */
-        return 0;
+        const startPos: number = this.findStartOfPacket(14, this.reader.line);
+        return startPos;
     }
 
     isStartOfPacket(buffer: string): boolean {
@@ -46,10 +55,49 @@ export class Day06 {
 
     }
 
-    // parseInputPart1(): void {
-    //     this.input = this.reader.lines.map((line: string): any => {
-    //         return 'something';
-    //     });
-    // }
+    findStartOfPacket(targetLength: number, buffer: string): number {
+        // Search through the buffer until we find 'targetLength' consecutive
+        // characters with NO matches
 
+        let uniqueCharactersMap: Map<string, number> = new Map();
+
+        let pos: number = 0; // start at the beginning
+
+        while (true) {  // do until we find something
+            // get the next character off the buffer
+            let char: string = buffer.charAt(pos);
+            if (uniqueCharactersMap.has(char)) {
+                // this character is a dupe! Clean out the map and continue
+
+                // We want to jump the map ahead to just after the PREVIOUS
+                // instance of this char.
+                // Get the position of the first instance
+                const firstPos: number = uniqueCharactersMap.get(char) as number; // it's always going to exist
+
+                // Remove everything with a 'pos' BEFORE that from the map
+                uniqueCharactersMap = MapUtils.filter(uniqueCharactersMap, (char, charPos): boolean => {
+                    // keep entries where the filter is TRUE
+                    return (charPos > firstPos);
+                });
+                // Update the char's value with the current pos.
+                uniqueCharactersMap.set(char, pos);
+            } else {
+                // this is a new character
+                // add it to the map and keep going
+                uniqueCharactersMap.set(char, pos);
+            }
+            // index forward
+            pos++;
+            // stop if we're done
+            if (uniqueCharactersMap.size == targetLength) break;
+        }
+        return pos;
+
+        // parseInputPart1(): void {
+        //     this.input = this.reader.lines.map((line: string): any => {
+        //         return 'something';
+        //     });
+        // }
+
+    }
 }
