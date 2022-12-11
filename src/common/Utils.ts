d4ea188138fa9f1cbb8a887a59f75dc93d927b0e
Justin Goeres <justin.goeres@gmail.com>
2022-12-11 11:17:15 -0500

    Day 11: Monkey in the Middle Part 1

--- src/common/Utils.ts
+++ src/common/Utils.ts
@@ -1,1 +1,7 @@
-export function sumReduce(sum:number,current:number):number {return sum + current;}
+export function sumReduce(sum: number, current: number): number {
+    return sum + current;
+}
+
+export function multiplyReduce(sum: number, current: number): number {
+    return sum * current;
+}
--- tests/day11.test.ts
+++ tests/day11.test.ts
@@ -1,20 +1,20 @@
 import {Day11} from '../src/2022/day11/Day11';
 
-describe('Day 11: NAME', () => {
+describe('Day 11: Monkey in the Middle', () => {
 
     let day11Example: Day11 = new Day11('data/day11/example.txt');
     let day11: Day11 = new Day11('data/day11/input.txt');
 
-    test('Example Part 1: PART1_RESULT', () => {
-        expect(day11Example.doPart1()).toBe(0);
+    test('Example Part 1: Monkey Business after 20 rounds', () => {
+        expect(day11Example.doPart1()).toBe(10605);
     });
 
     test('Example Part 2: PART2_RESULT', () => {
         expect(day11Example.doPart2()).toBe(0);
     });
 
-    test('Part 1: PART1_RESULT', () => {
-        expect(day11.doPart1()).toBe(0);
+    test('Part 1: Monkey Business after 20 rounds', () => {
+        expect(day11.doPart1()).toBe(56120);
     });
 
     test('Part 2: PART2_RESULT', () => {
