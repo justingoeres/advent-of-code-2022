b8ac8ec015ed2437c4383fcd635295ac487b45d7
Justin Goeres <justin.goeres@merklecxm.com>
2022-12-14 10:16:55 -0500

    Day 14: Regolith Reservoir Part 1

--- src/2022/day14/Day14.ts
+++ src/2022/day14/Day14.ts
@@ -1,28 +1,35 @@
 import {LinesReader} from '../../common/reader/LinesReader';
-import {XYPoint} from '../../common/Directions';
+import {URDL, XYPoint} from '../../common/Directions';
 
 export class Day14 {
     reader: LinesReader;
     input: any;
     rocks: Set<string> = new Set;
+    sand: Set<string> = new Set;
     xMax: number = 0;
     yMax: number = 0;
     xMin: number = Infinity;
     yMin: number = Infinity;
-    private readonly source: string = XYPoint.keyOf(500, 0);
+    private readonly source: XYPoint;
+    private readonly sourceKey: string;
 
     constructor(inputFile: string) {
         this.reader = new LinesReader(inputFile);
         this.reader.read();
         this.parseInput();
+        this.source = new XYPoint(500, 0);
+        this.sourceKey = this.source.keyString();
     }
 
     doPart1(): number {
         /*
             How many units of sand come to rest before sand starts flowing into the abyss below?
         */
+        while (this.generate1Sand()) ;
+        // print when done
+        this.printCave();
 
-        return 0;
+        return this.sand.size;
     }
 
     doPart2(): number {
@@ -30,6 +37,65 @@ export class Day14 {
             DESCRIPTION
         */
         return 0;
+    }
+
+    generate1Sand(): boolean {
+        /* RULES
+        Sand is produced one unit at a time, and the next unit of sand is not produced until the previous unit of sand comes to rest.
+
+        A unit of sand always falls down one step if possible.
+        If the tile immediately below is blocked (by rock or sand), the unit of sand attempts to instead move diagonally one step down and to the left.
+        If that tile is blocked, the unit of sand attempts to instead move diagonally one step down and to the right.
+        Sand keeps moving as long as it is able to do so,
+         */
+        const sand: XYPoint = new XYPoint(this.source.x, this.source.y); // start at the origin
+
+        // move that sand!
+        let isBlocked: boolean = false;
+        while (!isBlocked) {
+            // go until we can't go anymore
+            if (!this.isBlocked((sand.getRelative(URDL.DOWN) as XYPoint).keyString())) {
+                // Try DOWN
+                sand.moveRelative(URDL.DOWN);
+            } else if (!this.isBlocked(((sand
+                // Try DOWN-LEFT
+                .getRelative(URDL.DOWN) as XYPoint)
+                .getRelative(URDL.LEFT) as XYPoint).keyString())) {
+                sand.moveRelative(URDL.DOWN).moveRelative(URDL.LEFT);
+            } else if (!this.isBlocked(((sand
+                // Try DOWN-RIGHT
+                .getRelative(URDL.DOWN) as XYPoint)
+                .getRelative(URDL.RIGHT) as XYPoint).keyString())) {
+                sand.moveRelative(URDL.DOWN).moveRelative(URDL.RIGHT);
+            } else {
+                // we're blocked!
+                isBlocked = true;
+            }
+            // Check to see if the sand is falling into the abyss.
+            // Is its x-coordinate outside the boundary, or its y-coordinate greater than our max?
+            // if (!(sand.x >= this.xMin
+            //     && sand.x <= this.xMax
+            //     && sand.y <= this.yMax)) {
+            if (!(sand.x >= 0
+                && sand.x <= 600
+                && sand.y <= 200)) {
+                // out of bounds, stop & return
+                console.log('*** Sand falls into the abyss at ' + sand.toString());
+                return false;
+            }
+        }
+        // once we're blocked, stop moving and place the sand
+        this.sand.add(sand.keyString());
+
+        // if (this.sand.size % 100 == 0) {
+        //     this.printCave();
+        // }
+        // since this sand stopped, keep adding more!
+        return true;
+    }
+
+    isBlocked(targetKey: string): boolean {
+        return this.rocks.has(targetKey) || this.sand.has(targetKey);
     }
 
     parseInput(): void {
@@ -47,7 +113,7 @@ export class Day14 {
                 this.makeWall(xy1, xy2).forEach((xyKey: string) => this.rocks.add(xyKey));
             }
         });
-        this.printRocks();
+        this.printCave();
     }
 
     makeWall(xy1: XYPoint, xy2: XYPoint): Set<string> {
@@ -77,18 +143,21 @@ export class Day14 {
         return wallXYs;
     }
 
-    printRocks(): void {
+    printCave(): void {
         let output: string = '';
         for (let y = 0; y <= this.yMax; y++) {
             for (let x = this.xMin; x <= this.xMax; x++) {
                 const xyKey: string = XYPoint.keyOf(x, y);
                 if (this.rocks.has(xyKey)) output += '#';   // wall
-                else if (false) output += 'o';  // sand
-                else if (XYPoint.keyOf(x, y) == this.source) output += '+'; //source
+                else if (this.sand.has(xyKey)) output += '=';  // sand
+                else if (XYPoint.keyOf(x, y) == this.sourceKey) output += '+'; //source
                 else output += '.';
             }
             output += '\n';
         }
+        output += 'xMin: ' + this.xMin + '\txMax: ' + this.xMax + '\n';
+        output += 'yMin: ' + this.yMin + '\tyMax: ' + this.yMax + '\n';
+        output += 'Sand: ' + this.sand.size;
         console.log(output);
     }
 }
--- tests/day14.test.ts
+++ tests/day14.test.ts
@@ -5,19 +5,19 @@ describe('Day 14: Regolith Reservoir', () => {
     let day14Example: Day14 = new Day14('data/day14/example.txt');
 
     test('Example Part 1: Units of sand', () => {
-        expect(day14Example.doPart1()).toBe(0);
+        expect(day14Example.doPart1()).toBe(24);
     });
 
-    test('Example Part 2: PART2_RESULT', () => {
-        expect(day14Example.doPart2()).toBe(0);
-    });
-
-    // let day14: Day14 = new Day14('data/day14/input.txt');
-    //
-    // test('Part 1: Units of sand', () => {
-    //     expect(day14.doPart1()).toBe(0);
+    // test('Example Part 2: PART2_RESULT', () => {
+    //     expect(day14Example.doPart2()).toBe(0);
     // });
-    //
+
+    let day14: Day14 = new Day14('data/day14/input.txt');
+
+    test('Part 1: Units of sand', () => {
+        expect(day14.doPart1()).toBe(795);
+    });
+
     // test('Part 2: PART2_RESULT', () => {
     //     expect(day14.doPart2()).toBe(0);
     // });
